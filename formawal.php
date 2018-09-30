a7d93b161dd2238671b36a5f9b1056fb9394eb62
mikhsand <43511563+mikhsand@users.noreply.github.com>
2018-09-30 11:52:27 +0700

    formawal
    form login

--- formawal.php
+++ formawal.php
@@ -1,5 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+<h2>LOGIN</h2>
+<form action=" " method="POST">
+	<table>
+		<tr>
+			<td>Username :</td>
+			<td></td>
+			<td><input type="text" name="username" id="username"></td>
+		</tr>
+
+		<tr>
+			<td>Password :</td>
+			<td></td>
+			<td><input type="password" name="password" id="password"></td>
+		</tr>
+
+		<tr>
+			<td><input type="submit" name="submit" value="login"></td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
 <?php
-
-
-
-?>
+	include 'proses.php';
+?>	
